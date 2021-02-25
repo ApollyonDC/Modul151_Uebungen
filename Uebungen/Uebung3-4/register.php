0b46cd799170eaa6bad9335cecdaa0e943043632
ApollyonDC <basil_saleem@sluz.ch>
2021-02-25 12:13:02 +0100

    register angefangen

---
+++ Uebungen/Uebung3-4/register.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <form action="?" method="POST">
+    <input type="text" name="firstname" placeholder="Susi">
+    <input type="text" name="lastname" placeholder="Mustermann">
+    <input type="text" name="username" placeholder="username">
+    <input type="password" name="password" placeholder="password">
+    <input type="submit" value="register">
+    </form>
+</body>
+</html>
+
+<?PHP
+
+if(isset($_POST['submit'])){
+
+    $db = DB::get();
+
+    try {
+        $db->startTransaction();
+
+        $usernamevar = $_POST['username'];
+        $passwordvar = $_POST['password'];
+        $firstnamevar = $_POST['firstname'];
+        $lastnamevar = $_POST['lastname'];
+
+        $db->query("INSERT INTO logins(username, password, first_name, last_name) VALUES(:usernamevar, :passwordvar, :firstnamevar, :lastnamevar)");
+    }
+    catch (\Exception $e) {
+        $db->rollback();
+        throw $e;
+    }
+
+
+}
+
+INCLUDE 'db.php';
+?>
