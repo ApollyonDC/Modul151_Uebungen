b8f10152d12cadd30640c9d329c6fad1c1fa92cf
ApollyonDC <basil_saleem@sluz.ch>
2021-03-18 11:19:58 +0100

    create User funktion geschrieben

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+
+    public function createUser(){
+        if(isset($_POST['submit'])){
+
+            try {
+                DB::beginTransaction();
+                DB::table('logins')->insert(['username' => $_POST['username'], 'password' => password_hash($_POST['password'], PASSWORD_DEFAULT),
+                'firstname' => $_POST['firstname'],'lastname'=> $_POST['lastname']]);
+            }
+            catch (\Exception $e) {
+                DB::rollBack();
+                throw $e;
+            }
+        }
+    }
 }
